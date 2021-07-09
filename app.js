18f2960e10c8aa2d260d433c83faa8edfc7b3975
KyAnh2001 <86228617+KyAnh2001@users.noreply.github.com>
2021-07-09 21:12:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -132,18 +132,83 @@
 //         }
 //     }
 // 5
-// let day = prompt("Ngày:");
-// let month = prompt("Tháng:");
-// let year = prompt("Năm:");
+// let day = Number(prompt("Ngày:"));
+// let month = Number(prompt("Tháng:"));
+// let year = Number(prompt("Năm:"));
+// function laNamNhuan(nYear)
+// {
+// 	if ((nYear % 4 == 0 && nYear % 100 != 0) || nYear % 400 == 0)
+// 	{
+// 		return true;
+// 	}
+// 	return false;
+// }
+// function tsongaytrongthang(nMonth, nYear)
+// {
+// 	let nNumOfDays;
+
+// 	switch (nMonth)
+// 	{
+// 	case 1:
+// 	case 3:
+// 	case 5:
+// 	case 7:
+// 	case 8:
+// 	case 10:
+// 	case 12: 
+// 		nNumOfDays = 31;
+// 		break;
+// 	case 4:
+// 	case 6:
+// 	case 9:
+// 	case 11: 
+// 		nNumOfDays = 30;
+// 		break;
+// 	case 2:
+// 		if (laNamNhuan(nYear)==true)
+// 		{
+// 			nNumOfDays = 29;
+// 		}
+// 		else
+// 		{
+// 			nNumOfDays = 28;
+// 		}
+// 		break;
+// 	}
+
+// 	return nNumOfDays;
+// }
+
+// function timNgayHomSau(nDay, nMonth, nYear)
+// {
+// 	nDay++;
+// 	if (nDay > tsongaytrongthang(nMonth, nYear))
+// 	{
+// 		nDay = 1;
+// 		nMonth++;
+// 		if (nMonth > 12)
+// 		{
+// 			nMonth = 1;
+// 			nYear++;
+// 		}
+// 	}
+// }
 // function ktNgayThangNam(ngay, thang, nam)
 // {
 //     let date = new Date(nam,thang-1,ngay);
 //     if (date.getFullYear() == nam && date.getMonth() + 1 == thang && date.getDate() == ngay) {
-//      alert(`Ngày ${day}/${month}/${year} là ngày hợp lệ`);
+//         return true;
 //     }
 //     else
 //     {
-//         alert(`Ngày ${day}/${month}/${year} là ngày không hợp lệ`);
+//         return false;
 //     }
 // }
-// console.log(ktNgayThangNam(day,month,year))
+// if(ktNgayThangNam(day,month,year)==true){
+//     alert(`Ngày ${day}/${month}/${year} là ngày hợp lệ`);
+//     timNgayHomSau(day,month,year);
+//     alert(`Ngày tiếp theo là: ${day}/${month}/${year}`);
+// }
+// else{
+//     alert(`Ngày ${day}/${month}/${year} là ngày không hợp lệ`);
+// }
